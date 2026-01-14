6883cee8eadd991d993594544abb20325546f14b
Francisco Garcia <franciscogarciapartida@gmail.com>
2026-01-14 20:55:43 +0100

    fusion proyectos

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.html',  // <--- Fíjate que apunta a tu archivo app.html
-  styleUrls: ['./app.css']    // <--- Y este a tu archivo app.css
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
-export class App {
-  title = 'frontend';
-}
+export class App {}
